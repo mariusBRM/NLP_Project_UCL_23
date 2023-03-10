235774863a64f7fab0dafe41af47089031e9a4a1
giuliominore <60554590+giuliominore@users.noreply.github.com>
2023-03-10 20:41:58 +0000

    added most bullet points (val, early stop, dropout

--- src/models/no_fine_tuning/model.py
+++ src/models/no_fine_tuning/model.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
-import ast
+from sklearn.metrics import accuracy_score, f1_score
 
 def preprocessing(embedding):
     """
@@ -23,29 +23,67 @@ def preprocessing(embedding):
 
 
 class Net(nn.Module):
-    def __init__(self,len_input_layer, len_output_layer):
+    def __init__(self,len_input_layer, len_output_layer, apply_dropout=False):
         super().__init__()
         
         self.fc1 = nn.Linear(len_input_layer,568)
         self.fc2 = nn.Linear(568, 366)
         self.fc3 = nn.Linear(366, 124)
         self.fc4 = nn.Linear(124, len_output_layer)
+        self.apply_dropout = apply_dropout
+        self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
+        if self.apply_dropout:
+            x = self.dropout(x)
         x = F.relu(self.fc2(x))
+        if self.apply_dropout:
+            x = self.dropout(x)
         x = F.relu(self.fc3(x))
+        if self.apply_dropout:
+            x = self.dropout(x)
         x = F.relu(self.fc4(x))
         return x
     
 
-def training_model(nb_epochs,train_dataloader,model,optimizer,loss):
+
+def compute_metrics(dataloader, model, criterion):
+    """
+    
+    This function returns the accuracy of the model on the data given in inputs
+    """
+
+    list_labels = []
+    list_pred = []
+
+    loss = 0.0
+    for (inputs, labels) in dataloader:
+        outputs = model.forward(inputs)
+        loss += criterion(outputs, labels).item()
+        _,predicted = torch.max(outputs, 1)
+        list_labels.append(labels)
+        list_pred.append(predicted)
+
+    y_true = torch.cat(list_labels).numpy()
+    y_pred = torch.cat(list_pred).numpy()
+    
+    loss = loss / len(dataloader)
+    acc = accuracy_score(y_true, y_pred) * 100
+    macro_f1 = f1_score(y_true, y_pred, average='macro')
+    
+    return loss, acc, macro_f1
+
+
+
+def training_model(nb_epochs, train_dataloader, val_dataloader, model, optimizer, criterion, patience):
     """
     This function trains the model on training data
     """
-    list_running_loss = []
 
     for epoch in range(nb_epochs):
+
+        model.train()
         running_loss = 0
         for j, data in enumerate(train_dataloader, 0):
 
@@ -62,36 +100,27 @@ def training_model(nb_epochs,train_dataloader,model,optimizer,loss):
             
             print('\rEpoch: {}\tbatch: {}\tLoss =  {:.3f}'.format(epoch, j, loss), end="")
 
-
-        print("\nRunning_loss = ", running_loss,)
+        running_loss = running_loss / len(train_dataloader)
         
-        #if loss from previous epoch and current loss are very close => we have converged
-        if epoch >=1 and np.abs(list_running_loss[-1] - running_loss) < 0.001:
-            break
+        # validation
+        model.eval()
+        with torch.no_grad():
+            val_loss, val_acc, val_macro_f1 = compute_metrics(dataloader=val_dataloader,network=model,criterion=criterion)
+            print("Epoch {}: train CE loss = {:.5f}".format(epoch+1, running_loss),
+                      '|| Valid: CE loss = {:.5f}   acc = {:.2f}%   macro-F1 = {:.4f}'.format(val_loss, val_acc, val_macro_f1))
 
-        list_running_loss.append(running_loss)
+        # early-stopping
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            dict_model = model.state_dict()
+            pat = 0
+        else:
+            pat += 1
+            if pat == patience:
+                print("Early Stopping: Validation Loss did not decrease for", patience, "epochs.")
+                break
 
-        print("")
-
-
-def compute_accuracy(Dataloader, model):
-    """
-    
-    This function returns the accuracy of the model on tha data given in inputs
-    """
-
-    nb_good_prediction = 0
-    total=0
-
-    for i,data in enumerate(Dataloader,0):
-        inputs, labels = data
-        outputs = model.forward(inputs)
-        _,predicted = torch.max(outputs, 1)
-        nb_good_prediction += predicted.eq(labels.data).cpu().sum()
-        total += labels.size(0)  
-        
-    return nb_good_prediction / total 
-
+    torch.save(dict_model, 'classification_no_fine_tuning.pt')
 
 
 
@@ -103,45 +132,62 @@ if __name__ == '__main__':
     all_labels = df["labels"].apply(lambda x : preprocessing(x)) 
     assert all_embeddings.shape[0] == all_labels.shape[0]
 
-
-
-
-    split = int(df.shape[0]*0.7)
-
-    #training data correspond to 70% of the original dataset
-    training_data = torch.stack(all_embeddings[:split].to_list())
-    training_labels = torch.stack(all_labels[:split].to_list())
-    assert len(training_data) == len(training_labels)
-
-    #test data correspond to 30% of the original dataset
-    test_data = torch.stack(all_embeddings[split:].to_list())
-    test_labels = torch.stack(all_labels[split:].to_list())
-    assert len(test_data) == len(test_labels)
-
+    all_embeddings = all_embeddings.to_list()
+    all_labels = all_labels.to_list()
 
     batch_size = 80
-    train_data = training_data
-    train_labels = training_labels
+
+    split = int(df.shape[0]*0.8)
+
+    #development data correspond to 80% of the original dataset
+    dev_data = all_embeddings[:split]
+    dev_labels = all_labels[:split]
+    assert len(dev_data) == len(dev_labels)
+
+    split_dev = int(len(dev_data)*0.9)
+    # train data is 90% of development data
+    train_data = torch.stack(dev_data[:split_dev])
+    train_labels = torch.stack(dev_labels[:split_dev])
+    assert len(train_data) == len(train_labels)
+    train_dataset = TensorDataset(train_data, train_labels)
+    train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
+
+    # validation data is 10% of development data
+    val_data = torch.stack(dev_data[split_dev:])
+    val_labels = torch.stack(dev_labels[split_dev:])
+    assert len(val_data) == len(val_labels)
+    val_dataset = TensorDataset(val_data, val_labels)
+    val_dataloader = DataLoader(val_dataset, batch_size, shuffle=False)
+
+    #test data correspond to 20% of the original dataset
+    test_data = torch.stack(all_embeddings[split:])
+    test_labels = torch.stack(all_labels[split:])
+    assert len(test_data) == len(test_labels)
+    test_dataset = TensorDataset(test_data, test_labels)
+    test_dataloader = DataLoader(test_dataset, batch_size, shuffle=False)
+
+
+    ## training
+
     nb_epochs = 500
-
-    model = Net(768,8)
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
-
-    #training data
-    my_dataset = TensorDataset(train_data, train_labels)
-    train_dataloader = DataLoader(my_dataset, batch_size, shuffle=True)
+    patience = 10
+    model = Net(768,8,apply_dropout=False)
     criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001)
+    # optim.Adam(model.parameters(), lr=0.001)
+    # optim.AdamW(model.parameters(), lr=0.001)
+    
 
-    training_model(nb_epochs, train_dataloader, model,optimizer,criterion)
+    training_model(nb_epochs, train_dataloader, val_dataloader, model, optimizer, criterion, patience)
 
-    #testing data
-    my_dataset = TensorDataset(test_data, test_labels)
-    test_dataloader = DataLoader(my_dataset, batch_size, shuffle=True)
-
-    #Accuracy 
+    # testing
     print("\n--------------------------------------------------------------------------------------\n")
-    print("Accuracy on training data : ", compute_accuracy(train_dataloader,model),"\n")
-    print("Accuracy on test data : ", compute_accuracy(test_dataloader,model),"\n")
+    train_loss, train_acc, train_macro_f1 = compute_metrics(train_dataloader,model,criterion)
+    print("Metrics on training data : CE loss = {:.5f} | acc = {:.2f}% | macro-F1 = {:.4f}".format(train_loss, train_acc, train_macro_f1),"\n")
+    val_loss, val_acc, val_macro_f1 = compute_metrics(val_dataloader,model,criterion)
+    print("Metrics on validation data : CE loss = {:.5f} | acc = {:.2f}% | macro-F1 = {:.4f}".format(val_loss, val_acc, val_macro_f1),"\n")
+    test_loss, test_acc, test_macro_f1 = compute_metrics(test_dataloader,model,criterion)
+    print("Metrics on test data : CE loss = {:.5f} | acc = {:.2f}% | macro-F1 = {:.4f}".format(test_loss, test_acc, test_macro_f1),"\n")
     print("--------------------------------------------------------------------------------------\n")
 
 
