ad03ef53843db90a02e0673273a9282ea5aa6747
mariusBRM <marius.reymauzaize@gmail.com>
2023-03-18 16:41:48 +0000

    [CLEANING] cleaning workspace

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -3,17 +3,3 @@ import pandas as pd
 
 
 # we want a dataset of the form
-
-# text, bias type, hatespeech
-def data_from_hugging_face(df):
-    feature = [
-        'hate_speech_score',
-        'text',
-        'target_race',
-        'target_religion',
-        'target_gender'
-    ]
-    df = df[feature]
-    new_df = df[(df['target_race'] == True) | (df['target_religion'] == True) | (df['target_gender'] == True)]
-
-    return new_df
