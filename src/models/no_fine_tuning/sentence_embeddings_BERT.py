aa3c5a1cdd05ab27b34d27d67ac7f641b78408e5
ruhayat <112715191+ruhayat@users.noreply.github.com>
2023-03-28 18:35:23 +0100

    final version sentence embeddings

--- src/models/no_fine_tuning/sentence_embeddings_BERT.py
+++ src/models/no_fine_tuning/sentence_embeddings_BERT.py
@@ -1,8 +1,16 @@
 from transformers import BertModel
 import torch
 import pickle
-from tokenize_BERT import id_comments, input_ids, attention_masks, labels, bert_model_name
+from tokenize_BERT import train_data, val_data, test_data, bert_model_name
 from csv import writer
+from torch.utils.data import TensorDataset, DataLoader
+
+
+if torch.backends.mps.is_available():
+    device = mps_device = torch.device("mps")
+else:
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
 
 def get_sentence_embeddings(model,list_input_ids,list_attention_mask):
@@ -32,84 +40,67 @@ def get_sentence_embeddings(model,list_input_ids,list_attention_mask):
     return sentence_embeddings
 
 
-if __name__ == '__main__':
 
-    
-    # Load model
-    model = BertModel.from_pretrained(bert_model_name, output_hidden_states = True)
+def store_embeddings_in_a_file(path_to_file, data, model):
+    """
+    path_to_file : path to the csv file where the embbedings will be stored
+    data : (id_comments, inputs_id, attention_mask, labels)
+    """
 
     # put the model in evaluation mode
     model.eval()
 
+    (id_comments, inputs_id, attention_masks, labels) = data
+    attention_masks = attention_masks.to(device)
 
-    dico = dict()
-
-    nb_data = input_ids.shape[0]
     batch_size = 200
+    dataset = TensorDataset(inputs_id, labels)
+    dataloader = DataLoader(dataset, batch_size = batch_size, shuffle=False)
 
-    nb_full_elements_in_dico = nb_data // batch_size  #nb elements in the dico that have 200 embeddings
     
 
-    with torch.no_grad():
-        for i in range(0,nb_full_elements_in_dico):
-        
-            #First, we are gonna run the bert model nb_elements_in_dico times. Each time, the model takes batch_size embeddings as inputs
-        
-            print(i)
-            sentence_embeddings = get_sentence_embeddings(model, input_ids[i*batch_size:(i+1)*batch_size],attention_masks[i*batch_size:(i+1)*batch_size])
-            dico[i+1] = sentence_embeddings
+    with open(path_to_file, 'a') as f_object:
 
-        #then, if there are some remaining embeddings, the model will take remaining_elements embeddings as inputs.
-        remaining_embeddings = nb_full_elements_in_dico % batch_size
-
-        if remaining_embeddings > 0:
-            #We take care of the remaining embeddings. 
-            sentence_embeddings = get_sentence_embeddings(model,input_ids[(i+1)*batch_size:],attention_masks[(i+1)*batch_size:])
-        dico[i+2] = sentence_embeddings
-
-    #shape of dico values : (batch_size,768) for the first nb_elements_in_dico elements and (remaining_embeddings,768) for the last element
-
-
-
-
-    #copy results in csv file
-    with open('../../../data/hs_sentence_embeddings_no_fine_tuning.csv', 'a') as f_object:
-        
-        #wite title
+        #write title
         L = ["id_comments", "embedding", "labels"]
         writer_object = writer(f_object)
         writer_object.writerow(L)
 
 
+        with torch.no_grad():
+            for i, data in enumerate(dataloader, 0):
+                mini_batch_inputs_id, mini_batch_labels = data
+                mini_batch_inputs_id = mini_batch_inputs_id.to(device)
+                mini_batch_labels = mini_batch_labels.to(device)
+                
+                sentence_embeddings = get_sentence_embeddings(model, mini_batch_inputs_id,attention_masks[i*batch_size:(i+1)*batch_size])
 
-        for i in range(0,nb_full_elements_in_dico):
-            #print(i)
-        
-            list_ids = id_comments[i*batch_size : (i+1)*batch_size].to_list() #all id_comments for a batch
-            list_labels = labels[i*batch_size : (i+1)*batch_size] #all labels for a batch 
-            list_embedd = dico[i+1] #all embeddings for a batch
 
-            for j in range(batch_size):
+                size_batch = mini_batch_inputs_id.shape[0]
 
-                L = [list_ids[j],list_embedd[j],list_labels[j]]
-                writer_object = writer(f_object)
-                writer_object.writerow(L)
-    
-        if remaining_embeddings > 0 : 
+                list_ids = id_comments[i*size_batch : (i+1)*size_batch].to_list() #all id_comments for a batch
+                
 
-            list_ids = id_comments[(i+1)*200 : ].to_list() #all remaining id_comments
-            list_labels = labels[(i+1)*200 :] #all remaining labels 
-            list_embedd = dico[i+2] #all embeddings for a batch
+                for j in range(size_batch):
 
-            for j in range(len(list_ids)): 
+                    L = [list_ids[j],sentence_embeddings[j],mini_batch_labels[j]]
+                    writer_object = writer(f_object)
+                    writer_object.writerow(L)
+                
+                
+                
+    # Close the file object
+    f_object.close()
 
-                L = [list_ids[j],list_embedd[j],list_labels[j]]
-                writer_object = writer(f_object)
-                writer_object.writerow(L)
 
-        
-        # Close the file object
-        f_object.close()
+if __name__ == '__main__':
+
+    model = BertModel.from_pretrained(bert_model_name, output_hidden_states = True)
+    model.to(device)
+
+    store_embeddings_in_a_file('../../../data/sentence_embeddings_no_fine_tuning_train__.csv', train_data, model)
+    store_embeddings_in_a_file('../../../data/sentence_embeddings_no_fine_tuning_val.csv', val_data, model)
+    store_embeddings_in_a_file('../../../data/sentence_embeddings_no_fine_tuning_test.csv', test_data, model)
 
 
 
