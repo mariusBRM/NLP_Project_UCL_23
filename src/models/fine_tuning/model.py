51eb4f78974e45e1a042e434683d355b3cf3374d
giuliominore <60554590+giuliominore@users.noreply.github.com>
2023-03-30 14:28:31 +0100

    rm one-hot from test fn

--- src/models/fine_tuning/model.py
+++ src/models/fine_tuning/model.py
@@ -149,19 +149,16 @@ def training_model(nb_epochs, train_dataloader, val_dataloader, patience):
 
 if __name__ == '__main__':
 
-    train_data, val_data, test_data = tokenize_BERT()
+    train_data, val_data, _ = tokenize_BERT()
     WEIGHTS = 1 / (torch.sqrt(torch.unique(train_data[3], return_counts = True)[1])).to('cuda')
 
 
     # create datasets
     train_dataset = TensorDataset(train_data[1],train_data[2], train_data[3])
     val_dataset = TensorDataset(val_data[1], val_data[2], val_data[3])
-    test_dataset = TensorDataset(test_data[1], test_data[2], test_data[3])
     batch_size = 4
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
-    # test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
-
 
     # summary get all info about performance
     summary = training_model(nb_epochs = 10, train_dataloader = train_loader, val_dataloader = val_loader, patience = 5)
